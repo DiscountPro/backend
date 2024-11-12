e3c4486649bb3c0819ee97a1a0ed35a9d8ba58e0
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-12 00:01:08 -0500

    feat(profile-queries): add query profile.

---
+++ web-service/src/main/java/com/discount_pro/web_service/profiles/domain/model/queries/GetProfileByRazonSocialQuery.java
@@ -0,0 +1,4 @@
+package com.discount_pro.web_service.profiles.domain.model.queries;
+
+public record GetProfileByRazonSocialQuery(String razonSocial) {
+}
---
+++ web-service/src/main/java/com/discount_pro/web_service/profiles/domain/model/queries/GetProfileByRoleQuery.java
@@ -0,0 +1,6 @@
+package com.discount_pro.web_service.profiles.domain.model.queries;
+
+import com.discount_pro.web_service.profiles.domain.model.valueobjects.Role;
+
+public record GetProfileByRoleQuery(Role role) {
+}
