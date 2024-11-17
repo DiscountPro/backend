142151b2dbd063f7447eaef053f8b923b7f6635d
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-17 15:48:01 -0500

    feat(profile-resource): update create profile resource.

--- src/main/java/com/discount_pro/web_service/profiles/interfaces/rest/resources/CreateProfileResource.java
+++ src/main/java/com/discount_pro/web_service/profiles/interfaces/rest/resources/CreateProfileResource.java
@@ -1,6 +1,7 @@
 package com.discount_pro.web_service.profiles.interfaces.rest.resources;
 
 import com.discount_pro.web_service.profiles.domain.model.valueobjects.Role;
+import com.discount_pro.web_service.profiles.domain.model.valueobjects.UserId;
 
-public record CreateProfileResource(String ruc, String razonSocial, Role role) {
+public record CreateProfileResource(String ruc, String razonSocial, Role role, UserId userId) {
 }
