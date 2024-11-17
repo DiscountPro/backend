153606e73e86db5a40d78d35b2c74d5bdba51ceb
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-17 15:57:16 -0500

    feat(iam-user-queries). add query.

---
+++ src/main/java/com/discount_pro/web_service/iam/domain/model/queries/GetRoleByNameQuery.java
@@ -0,0 +1,6 @@
+package com.discount_pro.web_service.iam.domain.model.queries;
+
+import com.discount_pro.web_service.iam.domain.model.valueobjects.Roles;
+
+public record GetRoleByNameQuery(Roles name) {
+}
