20501d5ea0ee9c237229995188ff071513a53ed5
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-17 15:37:24 -0500

    feat(profile-query): add get profile by user id.

---
+++ src/main/java/com/discount_pro/web_service/profiles/domain/model/queries/GetProfileByUserIdQuery.java
@@ -0,0 +1,6 @@
+package com.discount_pro.web_service.profiles.domain.model.queries;
+
+import com.discount_pro.web_service.profiles.domain.model.valueobjects.UserId;
+
+public record GetProfileByUserIdQuery(UserId userId) {
+}
