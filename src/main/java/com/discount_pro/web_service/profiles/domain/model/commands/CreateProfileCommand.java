1a9271da3b926ab5612ca07917092a2c60f64944
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-17 15:42:18 -0500

    feat(profile-command): updating the profile creation command by adding the user id.

--- src/main/java/com/discount_pro/web_service/profiles/domain/model/commands/CreateProfileCommand.java
+++ src/main/java/com/discount_pro/web_service/profiles/domain/model/commands/CreateProfileCommand.java
@@ -1,6 +1,7 @@
 package com.discount_pro.web_service.profiles.domain.model.commands;
 
 import com.discount_pro.web_service.profiles.domain.model.valueobjects.Role;
+import com.discount_pro.web_service.profiles.domain.model.valueobjects.UserId;
 
-public record CreateProfileCommand(String RUC, String razonSocial, Role role) {
+public record CreateProfileCommand(String RUC, String razonSocial, Role role, UserId userId) {
 }
