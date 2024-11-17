88264f4d5c7e169e90f1f19b93f8c21f3db378fd
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-17 15:46:04 -0500

    feat(profile-repository): add exists razon social.

--- src/main/java/com/discount_pro/web_service/profiles/infrastructure/persistence/jpa/repositories/ProfileRepository.java
+++ src/main/java/com/discount_pro/web_service/profiles/infrastructure/persistence/jpa/repositories/ProfileRepository.java
@@ -2,6 +2,7 @@ package com.discount_pro.web_service.profiles.infrastructure.persistence.jpa.rep
 
 import com.discount_pro.web_service.profiles.domain.model.aggregates.Profile;
 import com.discount_pro.web_service.profiles.domain.model.valueobjects.Role;
+import com.discount_pro.web_service.profiles.domain.model.valueobjects.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,10 @@ import java.util.Optional;
 public interface ProfileRepository extends JpaRepository<Profile,Long> {
     boolean existsByRazonSocial(String razonSocial);
     boolean existsByRazonSocialAndIdIsNot(String razonSocial, Long id);
+    boolean existsByRuc(String RUC);
     Optional<Profile> findByRazonSocial(String razonSocial);
     List<Profile> findByRole(Role role);
+    Optional<Profile> findByUserId(UserId userId);
+
+    //boolean existsByRuc(String ruc);
 }
