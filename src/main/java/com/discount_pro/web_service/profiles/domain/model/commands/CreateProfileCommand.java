e46adf0031d2f15d51c6199f29c6692535f03913
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-23 13:56:12 -0500

    feat(profile-aggregates): update parameter create profile command.

--- src/main/java/com/discount_pro/web_service/profiles/domain/model/commands/CreateProfileCommand.java
+++ src/main/java/com/discount_pro/web_service/profiles/domain/model/commands/CreateProfileCommand.java
@@ -1,7 +1,7 @@
 package com.discount_pro.web_service.profiles.domain.model.commands;
 
 import com.discount_pro.web_service.profiles.domain.model.valueobjects.Role;
-import com.discount_pro.web_service.profiles.domain.model.valueobjects.UserId;
+//import com.discount_pro.web_service.profiles.domain.model.valueobjects.UserId;
 
-public record CreateProfileCommand(String RUC, String razonSocial, Role role, UserId userId) {
+public record CreateProfileCommand(String userName,String password,String RUC, String razonSocial, Role role) {
 }
